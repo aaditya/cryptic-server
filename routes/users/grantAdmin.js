69d336b6e5af3ad40018b7ad00439a5eb08ec5a2
Aaditya Chakravarty <ac36500@gmail.com>
2020-08-14 20:37:06 +0530

    Remove Admin Access Update

--- routes/users/grantAdmin.js
+++ routes/users/grantAdmin.js
@@ -2,13 +2,13 @@ const User = require('../../models/user');
 
 const getUserHandler = async (req, res, next) => {
     try {
-        let { id } = req.body;
+        let { id, status } = req.body;
 
-        if (!id) {
+        if (!id || !status) {
             return res.status(400).json({ "message": "Details Invalid" });
         }
 
-        let users = await User.findOneAndUpdate({ _id: id }, { $set: { access: "admin" } });
+        let users = await User.findOneAndUpdate({ _id: id }, { $set: { access: status } });
 
         if (!users) {
             return res.status(404).json({ "message": "No Users in system" })
